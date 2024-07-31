5bba93e329387e11cd586e187276e521f226c527
Sofia Donato Ferreira <sofia.ferreira@lnls.br>
2024-07-31 16:28:42 -0300

    refactor!: switch from automatic generation to manual magics creation
    Signed-off-by: Sofia Donato Ferreira <sofia.ferreira@lnls.br>

--- src/sophys/cli/extensions/common_ext.py
+++ src/sophys/cli/extensions/common_ext.py
@@ -1,16 +1,125 @@
 from argparse import ArgumentParser
 
+import functools
 import importlib
 import inspect
-import math
-import typing
-
-from pydantic import validate_call
 
 from IPython.core.magic import Magics, magics_class, record_magic, needs_local_scope
 
+from sophys.common.utils.registry import find_all as registry_find_all
 
-__INS_P = inspect.Parameter
+
+class PlanCLI:
+    def __init__(self, plan_name: str, plan):
+        self._plan_name = plan_name
+        self._plan = plan
+
+        assert self._plan_name == self._plan.__name__
+
+        self._sent_help_message = False
+
+    def get_real_devices(self, device_names: list[str], local_ns):
+        """
+        Get the objects corresponding to a list of device names.
+
+        Returns
+        -------
+        A list of device objects, corresponding to the 'device_names' list.
+        """
+
+        real_devices = []
+
+        for dev_name in device_names:
+            if hasattr(local_ns["D"], dev_name):
+                real_devices.append(getattr(local_ns["D"], dev_name))
+                continue
+
+            try:
+                dev = registry_find_all(name=dev_name)[0]
+            except Exception:
+                pass
+            else:
+                real_devices.append(dev)
+
+            raise Exception(f"Could not find detector with name '{dev_name}'.")
+
+        return real_devices
+
+    def create_parser(self):
+        def _on_exit_override(*_):
+            self._sent_help_message = True
+
+        _a = ArgumentParser(self._plan_name, description=inspect.getdoc(self._plan), add_help=True, exit_on_error=False)
+        _a.exit = _on_exit_override
+
+        return _a
+
+    def create_run_callback(self):
+        def __inner(parsed_namespace, local_ns):
+            if self._sent_help_message:
+                self._sent_help_message = False
+                return
+            return self._create_plan_gen(parsed_namespace, local_ns)
+
+        return __inner
+
+    def _create_plan_gen(self, parsed_namespace, local_ns):
+        pass
+
+
+class PlanCount(PlanCLI):
+    def __init__(self, plan):
+        super().__init__("count", plan)
+
+    def create_parser(self):
+        _a = super().create_parser()
+
+        _a.add_argument("-d", "--detectors", nargs='+', type=str)
+        _a.add_argument("-n", "--num", type=int, default=1)
+
+        return _a
+
+    def _create_plan_gen(self, parsed_namespace, local_ns):
+        detector = self.get_real_devices(parsed_namespace.detectors, local_ns)
+        num = parsed_namespace.num
+
+        return functools.partial(self._plan, detector, num=num)
+
+
+class PlanScan(PlanCLI):
+    def __init__(self, plan):
+        super().__init__("scan", plan)
+
+    def create_parser(self):
+        _a = super().create_parser()
+
+        _a.add_argument("-d", "--detectors", nargs='+', type=str)
+        _a.add_argument("-m", "--motors", nargs='+', type=str)
+        _a.add_argument("-n", "--num", type=int, default=1)
+
+        return _a
+
+    def _create_plan_gen(self, parsed_namespace, local_ns):
+        detector = self.get_real_devices(parsed_namespace.detectors, local_ns)
+        args = []
+        motors_str_list = parsed_namespace.motors
+        for i in range(0, len(motors_str_list) - 2, 3):
+            obj_str, start_str, end_str = motors_str_list[i:i+3]
+            args.append(self.get_real_devices([obj_str], local_ns)[0])
+            args.append(float(start_str))
+            args.append(float(end_str))
+
+        num = parsed_namespace.num
+        if len(motors_str_list) % 3 == 1:
+            num = int(motors_str_list[-1])
+
+        return functools.partial(self._plan, detector, *args, num=num)
+
+
+PLAN_WHITELIST = {
+    "count": PlanCount,
+    "scan": PlanScan,
+}
 
 
 @magics_class
@@ -18,162 +127,22 @@ class RealMagics(Magics):
     ...
 
 
-def get_parameter_type(param: __INS_P) -> tuple[type, str, bool]:
-    """
-    Get the type and number of a parameter.
-
-    Returns
-    -------
-    Tuple of:
-        type of parameter
-        nargs
-        whether to transform into a single number if we have a list of one item
-    """
-    # Get type from the default value
-    if (def_type := type(param.default)) not in (type, type(None), __INS_P.empty):
-        return def_type, '?', False
-
-    # Get typing.(...) types
-    if isinstance(param.annotation, typing._GenericAlias):
-        # typing.Sequence[...]
-        if hasattr(param.annotation, "_name") and param.annotation._name == "Sequence":
-            if any(i in str(param.annotation.__args__) for i in ("float", "int")):
-                return float, '+', False
-            return str, '+', False
-
-        # Mostly typing.Union[...]
-        if any(i in str(param.annotation) for i in ("Sequence", "tuple")):
-            return float, '+', True
-        if any(i in str(param.annotation) for i in ("float", "int")):
-            return float, '?', False
-        return str, '?', False
-
-    # Fallback
-    nargs = '+' if param.kind in (__INS_P.VAR_POSITIONAL, __INS_P.VAR_KEYWORD) else '?'
-    return param.annotation, nargs, False
-
-
-def filter_maybe(type, iterable):
-    res = []
-    for i in iterable:
-        try:
-            res.append(type(i))
-        except Exception:
-            res.append(i)
-    return res
-
-
-def float_downcast(val):
-    if isinstance(val, bool):
-        raise Exception
-    return float(val)
-
-
-def int_downcast(val):
-    if not isinstance(val, float):
-        raise Exception
-    if math.ceil(val) != math.floor(val):
-        raise Exception
-    return int(val)
-
-
-def bind_and_process_mixed(plan_signature: inspect.Signature, *args, **kwargs):
-    ret_args = []
-    ret_kwargs = {}
-
-    for name, param in plan_signature.parameters.items():
-        if param.kind is __INS_P.POSITIONAL_ONLY:
-            if name in kwargs:
-                ret_args.append(kwargs[name])
-            elif len(args) > 0:
-                ret_args.append(args[0])
-                args = args[1:]
-        elif param.kind is __INS_P.VAR_POSITIONAL:
-            if name in kwargs:
-                ret_args.extend(i for i in kwargs[name])
-            elif len(args) > 0:
-                ret_args.extend(i for i in args[0])
-                args = args[1:]
-        else:
-            if name in kwargs:
-                ret_kwargs[name] = kwargs[name]
-            elif len(args) > 0:
-                ret_kwargs[name] = args[0]
-                args = args[1:]
-
-    for t in (float_downcast, int_downcast):
-        ret_args = filter_maybe(t, ret_args)
-        for name, val in ret_kwargs.items():
-            ret_kwargs[name] = filter_maybe(t, val) if isinstance(val, typing.Iterable) else filter_maybe(t, [val])[0]
-
-    return ret_args, ret_kwargs
-
-
 def register_magic_for_plan(plan_name, plan):
-    plan_inspect = inspect.signature(plan)
+    plan_cls = PLAN_WHITELIST[plan_name]
+    plan_obj = plan_cls(plan)
 
-    sent_help_message = False
-
-    def _on_exit_override(*_):
-        nonlocal sent_help_message
-        sent_help_message = True
-
-    _a = ArgumentParser(prog=plan_name, description=inspect.getdoc(plan), add_help=True, exit_on_error=False)
-    _a.exit = _on_exit_override
-
-    argument_data = {}
-
-    for p in plan_inspect.parameters.values():
-        default_val = p.default if p.default is not __INS_P.empty else None
-
-        val_type, nargs, cast_on_single_item = get_parameter_type(p)
-        argument_data[p.name] = cast_on_single_item
-
-        add_argument_params = {}
-
-        if default_val is not None:
-            add_argument_params["default"] = default_val
-
-        if val_type not in (type(None), __INS_P.empty):
-            add_argument_params["type"] = val_type
-
-        if nargs is not None:
-            add_argument_params["nargs"] = nargs
-
-        _a.add_argument(f"--{p.name}", **add_argument_params)
+    _a = plan_obj.create_parser()
+    run_callback = plan_obj.create_run_callback()
 
     @needs_local_scope
     def __inner(line, local_ns):
-        parsed_kw_args, parsed_pos_args = _a.parse_known_args(line.strip().split(' '))
-
-        nonlocal sent_help_message
-        if sent_help_message:
-            sent_help_message = False
-            return
-
-        pos_args = []
-        kw_args = {}
+        parsed_namespace, _ = _a.parse_known_args(line.strip().split(' '))
 
         try:
-            for arg in parsed_pos_args:
-                pos_args.append(getattr(local_ns["D"], str(arg), arg))
-
-            for name, arg in parsed_kw_args.__dict__.items():
-                if arg is None:
-                    continue
-
-                if isinstance(arg, typing.Iterable):
-                    if len(arg) == 1 and argument_data[name]:
-                        kw_args[name] = getattr(local_ns["D"], str(arg[0]), arg[0])
-                    else:
-                        kw_args[name] = [getattr(local_ns["D"], str(i), i) for i in arg]
-                else:
-                    kw_args[name] = arg
-
-            pos_args, kw_args = bind_and_process_mixed(plan_inspect, *pos_args, **kw_args)
-
-            plan_gen = validate_call(plan, config={"arbitrary_types_allowed": True})(*pos_args, **kw_args)
-            return local_ns["RE"](plan_gen)
+            plan_gen = run_callback(parsed_namespace, local_ns)
+            if plan_gen is None:
+                return
+            return local_ns["RE"](plan_gen())
         except TypeError as e:
             print()
             print("Failed to run the provided plan.")
@@ -190,6 +159,8 @@ def register_magic_for_plan(plan_name, plan):
 def get_plans(beamline: str):
     def __inner(module):
         for maybe_plan_name in dir(module):
+            if maybe_plan_name not in PLAN_WHITELIST:
+                continue
             maybe_plan = getattr(module, maybe_plan_name)
             if inspect.isgeneratorfunction(maybe_plan):
                 yield (maybe_plan_name, maybe_plan)
