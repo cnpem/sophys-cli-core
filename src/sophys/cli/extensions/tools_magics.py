f74680805789b22d602f30fcabd932bd5e8f9e5b
Sofia Donato Ferreira <sofia.ferreira@lnls.br>
2024-08-05 09:20:33 -0300

    feat: use default arguments in kbl magic when running with no user input
    Signed-off-by: Sofia Donato Ferreira <sofia.ferreira@lnls.br>

--- src/sophys/cli/extensions/tools_magics.py
+++ src/sophys/cli/extensions/tools_magics.py
@@ -1,12 +1,23 @@
+import logging
 import subprocess
 
-from IPython.core.magic import Magics, magics_class, line_magic
+from IPython.core.magic import Magics, magics_class, line_magic, needs_local_scope
 
 
 @magics_class
 class KBLMagics(Magics):
     @line_magic
-    def kbl(self, line):
+    @needs_local_scope
+    def kbl(self, line, local_ns):
         """Run kafka-bluesky-live in a subprocess."""
-        subprocess.Popen(["kbl", *line.split(" ")],
+        if len(line) > 0:
+            command_line = ["kbl", *line.split(" ")]
+        else:
+            default_bss = local_ns["default_bootstrap_servers"]()
+            default_tn = local_ns["default_topic_names"]()[0]
+            command_line = ["kbl", default_tn, "--bootstrap-servers", " ".join(default_bss)]
+
+        proc = subprocess.Popen(command_line,
                          stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+        logging.info(f"Running {command_line} in a new process... (PID={proc.pid})")
