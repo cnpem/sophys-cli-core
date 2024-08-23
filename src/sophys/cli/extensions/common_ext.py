10821ff5ce5b4f3ab1dffbde5bfb7c4480feb40c
Sofia Donato Ferreira <sofia.ferreira@lnls.br>
2024-08-23 09:44:45 -0300

    feat: add remote control support for 'common' ext
    My idea here is to at one point move both the manager and the hardcoded
    IP address to sophys-common or similar, such that this can go into
    pre-execution and be generic to all beamlines, similar to what we do with
    the kafka callbacks.
    
    But for now, a bit of hardcoding and duplication is needed.
    
    Signed-off-by: Sofia Donato Ferreira <sofia.ferreira@lnls.br>

--- src/sophys/cli/extensions/common_ext.py
+++ src/sophys/cli/extensions/common_ext.py
@@ -3,6 +3,8 @@ from .tools_magics import KBLMagics
 
 from .plan_magics import PlanMV, PlanCount, PlanScan, PlanGridScan
 
+from ..http_utils import RemoteSessionHandler
+
 
 PLAN_WHITELIST = {
     "mv": ("mov", PlanMV),
@@ -13,8 +15,11 @@ PLAN_WHITELIST = {
 
 
 def load_ipython_extension(ipython):
+    local_mode = ipython.user_ns.get("LOCAL_MODE", False)
+    mode_of_op = ModeOfOperation.Local if local_mode else ModeOfOperation.Remote
+
     for plan_name, plan in get_plans("common", PLAN_WHITELIST):
-        register_magic_for_plan(plan_name, plan, PLAN_WHITELIST, ModeOfOperation.Local)
+        register_magic_for_plan(plan_name, plan, PLAN_WHITELIST, mode_of_op)
     ipython.register_magics(RealMagics)
 
     ipython.register_magics(KBLMagics)
@@ -23,6 +28,13 @@ def load_ipython_extension(ipython):
     print("    kbl: Open kafka-bluesky-live")
     print("")
     print("")
+
+    if not local_mode:
+        remote_session_handler = RemoteSessionHandler("http://10.30.1.50:60610")
+        remote_session_handler.start()
+        remote_session_handler.ask_for_authentication()
+
+        ipython.push({"_remote_session_handler": remote_session_handler})
 
 
 def unload_ipython_extension(ipython):
