33aa875ed4ed3df887af8c9159002aed42ad1d50
Sofia Donato Ferreira <sofia.ferreira@lnls.br>
2024-09-24 12:05:26 -0300

    feat: add command to query server state
    Signed-off-by: Sofia Donato Ferreira <sofia.ferreira@lnls.br>

--- src/sophys/cli/extensions/tools_magics.py
+++ src/sophys/cli/extensions/tools_magics.py
@@ -81,9 +81,35 @@ class HTTPMagics(Magics):
             # We need to modify the original one, not the 'local_ns', which is a copy.
             get_ipython().push({"P": set(res["plans_allowed"]) & set(self.plan_whitelist)})
 
+    @line_magic
+    @needs_local_scope
+    def query_state(self, line, local_ns):
+        def pretty_print_state(state):
+            print()
+            print(f"Version: {state['msg']}")
+            print( "Running state:")  # noqa: E201
+            print(f"  Manager: {state['manager_state']}")
+            print(f"  RunEngine: {state['re_state']} (Exists: {state['worker_environment_exists']} | State: {state['worker_environment_state']})")
+            print(f"  Items:  Queue ({state['items_in_queue']}) | History ({state['items_in_history']})")
+            print( "Server configuration:")  # noqa: E201
+            print(f"  Pause pending: {state['pause_pending']} | Stop pending: {state['queue_stop_pending']}")
+            print(f"  Autostart: {state["queue_autostart_enabled"]}")
+            print(f"  Loop: {state['plan_queue_mode']['loop']}")
+            print()
+
+        remote_session_handler = local_ns.get("_remote_session_handler", None)
+        if remote_session_handler is None:
+            logging.debug("No '_remote_session_handler' variable present in local_ns.")
+            return
+
+        res = remote_session_handler.get_authorized_manager().status()
+
+        pretty_print_state(res)
+
     @staticmethod
     def description():
         tools = []
         tools.append(("reload_devices", "Reload the available devices list (D)."))
         tools.append(("reload_plans", "Reload the available plans list (P)."))
+        tools.append(("query_state", "Query the current server state."))
         return tools
