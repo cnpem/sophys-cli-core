bec72e3ac15bef4cac9a8191eb07d2b5dd114288
Sofia Donato Ferreira <sofia.ferreira@lnls.br>
2024-10-04 09:35:58 -0300

    refactor: use object instead of tuple for plan whitelist
    This will allow us to better handle extra values for specific extensions
    later down the line.
    
    Signed-off-by: Sofia Donato Ferreira <sofia.ferreira@lnls.br>

--- src/sophys/cli/extensions/__init__.py
+++ src/sophys/cli/extensions/__init__.py
@@ -30,3 +30,11 @@ def setup_remote_session_handler(ipython, address: str):
 
     ipython.run_line_magic("reload_devices", "")
     ipython.run_line_magic("reload_plans", "")
+
+
+def setup_plan_magics(ipython, sophys_name: str, plan_whitelist: dict, mode_of_operation):
+    from .plan_magics import register_magic_for_plan, get_plans, RealMagics
+
+    for plan_name, plan in get_plans(sophys_name, plan_whitelist.keys()):
+        register_magic_for_plan(plan, plan_whitelist[plan_name], mode_of_operation)
+    ipython.register_magics(RealMagics)
