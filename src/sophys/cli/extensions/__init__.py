f1e41506d98895324f60fe7c857f60aac8ccf90c
Sofia Donato Ferreira <sofia.ferreira@lnls.br>
2024-10-24 17:42:59 -0300

    feat: add support for post-plan submission procedures
    Signed-off-by: Sofia Donato Ferreira <sofia.ferreira@lnls.br>

--- src/sophys/cli/extensions/__init__.py
+++ src/sophys/cli/extensions/__init__.py
@@ -32,9 +32,12 @@ def setup_remote_session_handler(ipython, address: str):
     ipython.run_line_magic("reload_plans", "")
 
 
-def setup_plan_magics(ipython, sophys_name: str, plan_whitelist: dict, mode_of_operation):
+def setup_plan_magics(ipython, sophys_name: str, plan_whitelist: dict, mode_of_operation, post_submission_callbacks: list[callable] | None = None):
     from .plan_magics import register_magic_for_plan, get_plans, RealMagics
 
+    if post_submission_callbacks is None:
+        post_submission_callbacks = []
+
     for plan_information, plan in get_plans(sophys_name, plan_whitelist):
-        register_magic_for_plan(plan, plan_information, mode_of_operation)
+        register_magic_for_plan(plan, plan_information, mode_of_operation, post_submission_callbacks)
     ipython.register_magics(RealMagics)
